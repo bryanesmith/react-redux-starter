321c83566c955bcb2278891283f60108f40ee42e
Bryan Smith <bryanesmith@gmail.com>
2017-08-13 19:34:05 -0400

    Add action creator test using jest

--- src/scripts/actions/index.js
+++ src/scripts/actions/index.js
@@ -12,6 +12,9 @@ export function fetchWeather() {
 }
 */
 
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
 export const REQUEST_WEATHER = 'REQUEST_WEATHER';
 function requestWeather() {
   return {
@@ -28,8 +31,13 @@ function receiveWeather(json) {
   };
 }
 
+// See: https://developer.yahoo.com/weather/
+function weatherUrl(place) {
+  return `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${encodeURIComponent(place)}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
+}
+
 export function fetchWeather(place) {
-  const url = `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${encodeURIComponent(place)}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
+  const url = weatherUrl(place);
   return function (dispatch) {
     dispatch(requestWeather());
     return fetch(url)
