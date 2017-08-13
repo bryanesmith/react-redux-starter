c6d4bbdc168e6679e9cf2cf6aa6f980e6f33e02f
Bryan Smith <bryanesmith@gmail.com>
2017-08-13 13:59:33 -0400

    Adding comments

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import './styles/index.scss';
 
 const svgSample = 'http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg';
 
+// Adding text
 const root = document.querySelector('#root');
 root.innerHTML = 'foo';
 
+// Adding SVG
 const img = document.createElement('img');
 img.src = svgSample;
 img.classList.add('sample-svg');
